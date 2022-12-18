d40a9b4798bcb509cf7924f7cf827ce0d99583b1
Narwhalssss <114067296+Narwhalssss@users.noreply.github.com>
2022-12-18 13:26:39 +0700

    update index.html

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Please put your answer here
+var signButton = $(".signup-show");
+var modal = $('#signup-modal');
+var closeButton = $('#close-modal');
+
+closeButton.click(function() {
+    modal.hide(1000);
+});
+
+signButton.click(function() {
+    modal.show(1000);
+});
