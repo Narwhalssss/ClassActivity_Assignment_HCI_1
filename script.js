d5f061bb1eef77dee1679bf0d273a18300ed205b
Narwhalssss <114067296+Narwhalssss@users.noreply.github.com>
2022-12-20 15:24:49 +0700

    update

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var modal = $('#signup-modal');
 var closeButton = $('#close-modal');
 
 closeButton.click(function() {
-    '#signup-modal'.slideUp(1000);
+    modal.slideUp(1000);
 });
 
 signButton.click(function() {
-    '#signup-modal'.slideDown(1000);
+    modal.slideDown(1000);
 });
